10e58eda07f623f1916706419996b4991e7f9721
matthieu solente <yuya1@hotmail.fr>
2023-09-12 01:06:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,3 +72,23 @@ $('#btnClass').on('change',function() {
   updateValues();
 });
 })(jQuery);
+
+/***********************************************/
+/* COPY PASTE   */
+/***********************************************/
+function copyPaste(element) {
+    var $temp = $("<textarea>");
+    $("body").append($temp);
+    $temp.val($(element).parent().find('.copyPaste').text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+/***********************************************/
+/************ COPY TO CLIPBOARD FUNCTION *******/
+/***********************************************/
+
+  $('.copyPaste').wrap('<div class="copyPaste-block"></div>');   
+    $('.copyPaste-block').each(function(){     
+      $(this).append('<button type="button" class="copy" onclick="copyPaste(this)" aria-hidden="false" aria-label="Copy to clipboard"><span class="visually-hidden">Copy to Clipboard</span></button>');
+    });
